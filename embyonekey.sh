389dd6fc90bcdc9577ae21f7430bde6386429729
xie9967 <78606806+xie9967@users.noreply.github.com>
2021-03-06 20:59:53 +0800

    Update embyonekey.sh

--- embyonekey.sh
+++ embyonekey.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd /var/packages/EmbyServer/target/mono/bin
-curl https://raw.githubusercontent.com/s1oz/embyonekey/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
+curl https://raw.githubusercontent.com/xie9967/embyonekey/blob/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
 ./cert-sync /etc/ssl/certs/ca-certificates.crt
 cd /usr/local/etc/nginx/conf.d/*-*-*-*
-wget https://raw.githubusercontent.com/s1oz/embyonekey/master/user.conf
+wget https://raw.githubusercontent.com/xie9967/embyonekey/blob/master/user.conf
 nginx -s reload
 
